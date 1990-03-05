57777e3ed95c4efa756c2d71542821f8d471b259
Keith Bostic <bostic>
1990-03-05 11:37:46 -0800

    fix dflag for ANSI C variable scoping

--- usr.sbin/mtree/create.c
+++ usr.sbin/mtree/create.c
@@ -16,7 +16,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)create.c	5.3 (Berkeley) 9/6/89";
+static char sccsid[] = "@(#)create.c	5.4 (Berkeley) 3/5/90";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -237,6 +237,7 @@ stats(ip)
 
 shostats()
 {
+	extern int dflag;
 	register ID *p;
 	register mode_t cnt;
 	register u_long max;
