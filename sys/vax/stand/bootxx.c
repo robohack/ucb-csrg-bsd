896f83d2a3418404eb94a852a7198787b5cf610f
Kirk McKusick <mckusick>
1990-04-04 16:20:01 -0800

    delete unneeded header files

--- sys/vax/stand/bootxx.c
+++ sys/vax/stand/bootxx.c
@@ -3,12 +3,10 @@
  * All rights reserved.  The Berkeley software License Agreement
  * specifies the terms and conditions for redistribution.
  *
- *	@(#)bootxx.c	7.4 (Berkeley) 2/4/88
+ *	@(#)bootxx.c	7.5 (Berkeley) 4/4/90
  */
 
 #include "param.h"
-#include "inode.h"
-#include "fs.h"
 #include "vm.h"
 #include "reboot.h"
 #include <a.out.h>
