93bd94ec48b9e7984f8b819f06580df19f2e76c9
Keith Bostic <bostic>
1990-04-18 12:53:28 -0800

    sendmail security problems; bug report 4.3BSD/usr.lib/153 sendmail security problems; bug report 4.3BSD/usr.lib/133 sendmail security problems; bug report 4.3BSD/usr.lib/136

--- usr.sbin/sendmail/src/readcf.c
+++ usr.sbin/sendmail/src/readcf.c
@@ -17,7 +17,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)readcf.c	5.19 (Berkeley) 9/11/89";
+static char sccsid[] = "@(#)readcf.c	5.20 (Berkeley) 4/18/90";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -866,6 +866,7 @@ setoption(opt, val, safe, sticky)
 
 	  case 'u':		/* set default uid */
 		DefUid = atoi(val);
+		setdefuser();
 		break;
 
 	  case 'v':		/* run in verbose mode */
