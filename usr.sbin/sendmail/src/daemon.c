0e6dcbd9048cfa5e0a8df3450f4bbb4a684789b7
Keith Bostic <bostic>
1990-04-18 11:15:25 -0800

    don't break old makefiles; bug report 4.3BSD/usr.lib/114

--- usr.sbin/sendmail/src/daemon.c
+++ usr.sbin/sendmail/src/daemon.c
@@ -17,13 +17,13 @@
  */
 
 #include <errno.h>
-#include <sendmail.h>
+#include "sendmail.h"
 
 #ifndef lint
 #ifdef DAEMON
-static char sccsid[] = "@(#)daemon.c	5.32 (Berkeley) 4/16/90 (with daemon mode)";
+static char sccsid[] = "@(#)daemon.c	5.33 (Berkeley) 4/18/90 (with daemon mode)";
 #else
-static char sccsid[] = "@(#)daemon.c	5.32 (Berkeley) 4/16/90 (without daemon mode)";
+static char sccsid[] = "@(#)daemon.c	5.33 (Berkeley) 4/18/90 (without daemon mode)";
 #endif
 #endif /* not lint */
 
