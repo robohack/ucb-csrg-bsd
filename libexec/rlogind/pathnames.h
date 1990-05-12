722f8cd06bd44670678f40fe62f475dd227688f2
Keith Bostic <bostic>
1990-05-12 14:24:10 -0800

    needs paths.h

--- libexec/rlogind/pathnames.h
+++ libexec/rlogind/pathnames.h
@@ -14,7 +14,9 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- *	@(#)pathnames.h	5.2 (Berkeley) 5/9/89
+ *	@(#)pathnames.h	5.3 (Berkeley) 5/12/90
  */
+
+#include <paths.h>
 
 #define	_PATH_LOGIN	"/usr/bin/login"
