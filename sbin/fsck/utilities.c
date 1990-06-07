c57fa25d1af7ab4a7e37778eadce1abe2b2d98f7
Kirk McKusick <mckusick>
1990-06-06 16:11:26 -0800

    add FIFO as file type

--- sbin/fsck/utilities.c
+++ sbin/fsck/utilities.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)utilities.c	5.26 (Berkeley) 6/1/90";
+static char sccsid[] = "@(#)utilities.c	5.27 (Berkeley) 6/6/90";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -32,6 +32,7 @@ ftypeok(dp)
 	case IFCHR:
 	case IFLNK:
 	case IFSOCK:
+	case IFIFO:
 		return (1);
 
 	default:
