f9e2faa6313d284b42ba81b99dbfeaba3ab0e624
Keith Bostic <bostic>
1990-07-20 13:18:56 -0800

    added field name q_ruser; need to update NullAddress

--- usr.sbin/sendmail/src/main.c
+++ usr.sbin/sendmail/src/main.c
@@ -13,7 +13,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	5.30 (Berkeley) 6/29/90";
+static char sccsid[] = "@(#)main.c	5.31 (Berkeley) 7/20/90";
 #endif /* not lint */
 
 #define	_DEFINE
@@ -67,7 +67,7 @@ char		*FullName;	/* sender's full name */
 ENVELOPE	BlankEnvelope;	/* a "blank" envelope */
 ENVELOPE	MainEnvelope;	/* the envelope around the basic letter */
 ADDRESS		NullAddress =	/* a null address */
-		{ "", "", "" };
+		{ "", "", NULL, "" };
 
 /*
 **  Pointers for setproctitle.
