c2fc34737c6bad7696381285e43c9129110ad091
Eric Allman <eric>
1990-08-29 13:39:51 -0800

    from Eric Wassenaar at NIKHEF-H, Amsterdam: reset EF_OLDSTYLE bit when returning mail

--- usr.sbin/sendmail/src/version.c
+++ usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	5.64 (Berkeley) 6/1/90";
+static char sccsid[] = "@(#)version.c	5.65 (Berkeley) 8/29/90";
 #endif /* not lint */
 
-char	Version[] = "5.64";
+char	Version[] = "5.65";
