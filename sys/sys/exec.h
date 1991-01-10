1eca008a9f9158034316062ff9414950127ce561
William Jolitz <william>
1991-01-09 19:37:21 -0800

    trivial 386 ifdefs added

--- sys/sys/exec.h
+++ sys/sys/exec.h
@@ -3,14 +3,14 @@
  * All rights reserved.  The Berkeley software License Agreement
  * specifies the terms and conditions for redistribution.
  *
- *	@(#)exec.h	7.2 (Berkeley) 5/9/90
+ *	@(#)exec.h	7.3 (Berkeley) 1/9/91
  */
 
 /*
  * Header prepended to each a.out file.
  */
 struct exec {
-#if !defined(vax) && !defined(tahoe)
+#if !defined(vax) && !defined(tahoe) && !defined(i386)
 unsigned short	a_mid;		/* machine ID */
 unsigned short	a_magic;	/* magic number */
 #else
