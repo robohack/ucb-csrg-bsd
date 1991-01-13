516c9d770bc4df746991274bc38adc5a9cdca167
Keith Bostic <bostic>
1991-01-13 10:17:18 -0800

    Chris Torek demands that it return the yyparse() value.

--- lib/liby/main.c
+++ lib/liby/main.c
@@ -6,13 +6,10 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	5.2 (Berkeley) 5/15/90";
+static char sccsid[] = "@(#)main.c	5.3 (Berkeley) 1/13/91";
 #endif /* not lint */
 
 main()
 {
-	extern yyparse();
-
-	(void)yyparse();
-	return(0);
+	exit(yyparse());
 }
