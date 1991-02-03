b248aec10d173fdcfd046406bfe8575794ff9803
Keith Bostic <bostic>
1991-02-03 13:14:08 -0800

    ANSI C change: remove -> rm change temp file creation to use mkstemp, delete unused function/variable

--- usr.bin/mail/lex.c
+++ usr.bin/mail/lex.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)lex.c	5.21 (Berkeley) 6/25/90";
+static char sccsid[] = "@(#)lex.c	5.22 (Berkeley) 2/3/91";
 #endif /* not lint */
 
 #include "rcv.h"
@@ -111,7 +111,7 @@ setfile(name)
 		perror(tempMesg);
 		exit(1);
 	}
-	remove(tempMesg);
+	rm(tempMesg);
 	setptr(ibuf);
 	setmsize(msgCount);
 	Fclose(ibuf);
