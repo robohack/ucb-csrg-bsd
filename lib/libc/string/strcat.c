5b5fdf20467b811f4c43c3dae2e0fb8f6d911f56
Keith Bostic <bostic>
1991-02-24 13:57:42 -0800

    Add include files to get prototype declarations, and fix bugs found.

--- lib/libc/string/strcat.c
+++ lib/libc/string/strcat.c
@@ -6,14 +6,15 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)strcat.c	5.5 (Berkeley) 6/1/90";
+static char sccsid[] = "@(#)strcat.c	5.6 (Berkeley) 2/24/91";
 #endif /* LIBC_SCCS and not lint */
 
 #include <string.h>
 
 char *
 strcat(s, append)
-	register char *s, *append;
+	register char *s;
+	register const char *append;
 {
 	char *save = s;
 
