b262a6b9a7e8706dc50cb9000aeed00eb6c5de09
Keith Bostic <bostic>
1991-03-01 14:07:21 -0800

    ANSI

--- usr.bin/from/from.c
+++ usr.bin/from/from.c
@@ -12,7 +12,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)from.c	5.6 (Berkeley) 7/21/90";
+static char sccsid[] = "@(#)from.c	5.7 (Berkeley) 3/1/91";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -27,7 +27,7 @@ main(argc, argv)
 {
 	extern char *optarg;
 	extern int optind;
-	struct passwd *pwd, *getpwuid();
+	struct passwd *pwd;
 	int ch, newline;
 	char *file, *sender, *p;
 #if MAXPATHLEN > BUFSIZ
