699ebe4158bf9db61db49876df0ea051c2e2ac81
Dave Borman <dab>
1991-03-01 01:35:10 -0800

    Telnet AUTHENTICATION option Telnet ENCRYPT option (test version) Function prototypes Bug fixes and code cleanup

--- libexec/telnetd/telnetd.h
+++ libexec/telnetd/telnetd.h
@@ -4,12 +4,18 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)telnetd.h	5.2 (Berkeley) 6/1/90
+ *	@(#)telnetd.h	5.3 (Berkeley) 3/1/91
  */
 
 
 #include "defs.h"
 #include "ext.h"
+
+#ifdef	DIAGNOSTICS
+#define	DIAG(a,b)	if (diagnostic & (a)) b
+#else
+#define	DIAG(a,b)
+#endif
 
 /* other external variables */
 extern	char **environ;
