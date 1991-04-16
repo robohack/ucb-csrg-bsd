6492b66c63b5d2c1e6f9bf6c4df201e0c9e3f6a7
Kirk McKusick <mckusick>
1991-04-15 23:39:00 -0800

    add include of namei.h

--- sys/nfs/nfs_srvcache.c
+++ sys/nfs/nfs_srvcache.c
@@ -7,7 +7,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)nfs_srvcache.c	7.10 (Berkeley) 3/19/91
+ *	@(#)nfs_srvcache.c	7.11 (Berkeley) 4/16/91
  */
 
 /*
@@ -17,6 +17,7 @@
  */
 
 #include "param.h"
+#include "namei.h"
 #include "vnode.h"
 #include "mount.h"
 #include "kernel.h"
