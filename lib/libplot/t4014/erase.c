0aea9e817ddf891e0d9dc44785149897b9b70f3c
Keith Bostic <bostic>
1991-04-22 14:49:30 -0800

    new copyright; att/bsd/shared

--- lib/libplot/t4014/erase.c
+++ lib/libplot/t4014/erase.c
@@ -1,6 +1,13 @@
+/*-
+ * Copyright (c) 1983 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.proprietary.c%
+ */
+
 #ifndef lint
-static char sccsid[] = "@(#)erase.c	4.2 (Berkeley) 1/2/85";
-#endif
+static char sccsid[] = "@(#)erase.c	4.3 (Berkeley) 4/22/91";
+#endif /* not lint */
 
 extern int ohiy;
 extern int ohix;
--- lib/libplot/t4014/subr.c
+++ lib/libplot/t4014/subr.c
@@ -1,6 +1,13 @@
+/*-
+ * Copyright (c) 1983 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.proprietary.c%
+ */
+
 #ifndef lint
-static char sccsid[] = "@(#)subr.c	4.2 (Berkeley) 7/15/87";
-#endif
+static char sccsid[] = "@(#)subr.c	4.3 (Berkeley) 4/22/91";
+#endif /* not lint */
 
 #include <stdio.h>
 float obotx = 0.;
