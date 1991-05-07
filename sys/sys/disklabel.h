4b4d0b5947b66f579de2f95544303e59777feaca
William Jolitz <william>
1991-05-07 06:09:14 -0800

    MAXPARTITIONS enforced differently on i386; no need for bigger const

--- sys/sys/disklabel.h
+++ sys/sys/disklabel.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)disklabel.h	7.18 (Berkeley) 4/28/91
+ *	@(#)disklabel.h	7.19 (Berkeley) 5/7/91
  */
 
 /*
@@ -24,7 +24,6 @@
 #ifdef i386
 #define LABELSECTOR	1			/* sector containing label */
 #define LABELOFFSET	0			/* offset of label in sector */
-#define	MAXPARTITIONS	32
 #endif
 
 #ifndef	LABELSECTOR
