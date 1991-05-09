93c153834dd7839d1982daad7d05fb9f230cc943
Keith Sklower <sklower>
1991-05-09 10:46:27 -0800

    change name of file

--- sys/netiso/clnp_debug.c
+++ sys/netiso/clnp_debug.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)clnp_debug.c	7.6 (Berkeley) 5/8/91
+ *	@(#)clnp_debug.c	7.7 (Berkeley) 5/9/91
  */
 
 /***********************************************************
@@ -231,5 +231,5 @@ register struct sockaddr_iso *s;
 	return (iso_addr_b);
 }
 
-#include "subr_prf.sprintf"
+#include "clnp_sprintf.c"
 #endif	ARGO_DEBUG
