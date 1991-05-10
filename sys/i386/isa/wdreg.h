d5d188d2af07b0950c3222352e7b4eb5714f2a84
Keith Bostic <bostic>
1991-05-09 22:32:47 -0800

    new copyright; att/bsd/shared

--- sys/i386/isa/wdreg.h
+++ sys/i386/isa/wdreg.h
@@ -1,15 +1,14 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
+ * Copyright (c) 1991 The Regents of the University of California.
  * All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * William Jolitz.
  *
- * %sccs.include.noredist.c%
+ * %sccs.include.redist.c%
  *
- *	@(#)wdreg.h	5.1 (Berkeley) 4/24/90
+ *	@(#)wdreg.h	7.1 (Berkeley) 5/9/91
  */
-
 
 /*
  * Disk Controller register definitions.
