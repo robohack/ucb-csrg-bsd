b4f82bc0293f084c958b704ac02eb6d8dd21d5f7
Keith Bostic <bostic>
1991-05-09 22:16:49 -0800

    new copyright; att/bsd/shared

--- sys/i386/include/mtpr.h
+++ sys/i386/include/mtpr.h
@@ -1,3 +1,12 @@
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)mtpr.h	7.1 (Berkeley) 5/9/91
+ */
+
 /*
  * i386 processor register definitions
  */
--- sys/i386/include/specialreg.h
+++ sys/i386/include/specialreg.h
@@ -1,3 +1,12 @@
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)specialreg.h	7.1 (Berkeley) 5/9/91
+ */
+
 /*
  * 386 Special registers:
  */
