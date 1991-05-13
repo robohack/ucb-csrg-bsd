2b41a3f81c69aedf92ee028111ec28f3ea6856ba
William Jolitz <william>
1991-05-12 19:09:39 -0800

    ifdef KERNEL'ed the externs

--- sys/i386/include/pcb.h
+++ sys/i386/include/pcb.h
@@ -7,7 +7,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)pcb.h	5.9 (Berkeley) 5/9/91
+ *	@(#)pcb.h	5.10 (Berkeley) 5/12/91
  */
 
 /*
@@ -45,4 +45,6 @@ struct pcb {
 	int	pcb_cmap2;	/* XXX temporary PTE - will prefault instead */
 };
 
+#ifdef KERNEL
 struct pcb *curpcb;		/* our current running pcb */
+#endif
