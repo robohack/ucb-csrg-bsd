d80c6a5977a2035d7ea02cdbbda7b2a3a3292017
Keith Bostic <bostic>
1991-05-23 17:14:51 -0800

    print out "stdin", not "-"

--- bin/cat/cat.c
+++ bin/cat/cat.c
@@ -15,7 +15,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)cat.c	5.14 (Berkeley) 5/6/91";
+static char sccsid[] = "@(#)cat.c	5.15 (Berkeley) 5/23/91";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -88,7 +88,7 @@ cook_args(argv)
 	register FILE *fp;
 
 	fp = stdin;
-	filename = "-";
+	filename = "stdin";
 	do {
 		if (*argv) {
 			if (!strcmp(*argv, "-"))
@@ -176,7 +176,7 @@ raw_args(argv)
 	register int fd;
 
 	fd = fileno(stdin);
-	filename = "-";
+	filename = "stdin";
 	do {
 		if (*argv) {
 			if (!strcmp(*argv, "-"))
