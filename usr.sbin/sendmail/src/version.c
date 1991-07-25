11fb3a55fa7ff4beb603441f55f35bd8d51b0a28
Eric Allman <eric>
1991-07-25 10:12:05 -0800

    some compatibility with SunOS for future work here at Berkeley; change NO_WILDCARD_MX compilation flag to configuration option "w".

--- usr.sbin/sendmail/src/version.c
+++ usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	5.68 (Berkeley) 7/25/91";
+static char sccsid[] = "@(#)version.c	5.69 (Berkeley) 7/25/91";
 #endif /* not lint */
 
-char	Version[] = "5.68";
+char	Version[] = "5.69";
