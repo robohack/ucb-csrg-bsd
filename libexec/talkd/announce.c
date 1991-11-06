7ca1ad7260f33521a6c5c162318a56e1e54abfcc
Keith Bostic <bostic>
1991-11-06 08:27:16 -0800

    don't terminate with a period, then cut and paste doesn't work

--- libexec/talkd/announce.c
+++ libexec/talkd/announce.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)announce.c	5.11 (Berkeley) 8/1/91";
+static char sccsid[] = "@(#)announce.c	5.12 (Berkeley) 11/6/91";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -88,7 +88,7 @@ print_mesg(tty, tf, request, remote_machine)
 	sizes[i] = strlen(line_buf[i]);
 	max_size = max(max_size, sizes[i]);
 	i++;
-	(void)sprintf(line_buf[i], "talk: connection requested by %s@%s.",
+	(void)sprintf(line_buf[i], "talk: connection requested by %s@%s",
 		request->l_name, remote_machine);
 	sizes[i] = strlen(line_buf[i]);
 	max_size = max(max_size, sizes[i]);
