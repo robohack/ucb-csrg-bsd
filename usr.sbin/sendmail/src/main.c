3dcb9b12862a3a9e77993becd0473a86c19b4da4
Eric Allman <eric>
1992-01-18 07:57:12 -0800

    drop mode a (ARPA FTP) -- NCP is gone forever.....

--- usr.sbin/sendmail/src/main.c
+++ usr.sbin/sendmail/src/main.c
@@ -13,7 +13,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	5.43 (Berkeley) 1/5/92";
+static char sccsid[] = "@(#)main.c	5.44 (Berkeley) 1/18/92";
 #endif /* not lint */
 
 #define	_DEFINE
@@ -335,7 +335,6 @@ main(argc, argv, envp)
 				ExitStat = EX_USAGE;
 				break;
 # endif SMTP
-			  case MD_ARPAFTP:
 			  case MD_DELIVER:
 			  case MD_VERIFY:
 			  case MD_TEST:
@@ -723,7 +722,7 @@ main(argc, argv, envp)
 	initsys();
 	setsender(from);
 
-	if (OpMode != MD_ARPAFTP && *av == NULL && !GrabTo)
+	if (*av == NULL && !GrabTo)
 	{
 		usrerr("Recipient names must be specified");
 
