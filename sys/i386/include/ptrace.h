ab1ae278ba51d5af012abb24cbc69c1ada4f996b
Kirk McKusick <mckusick>
1992-02-05 22:41:23 -0800

    date and time created 92/02/05 22:41:23 by mckusick

---
+++ sys/i386/include/ptrace.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 1992 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)ptrace.h	7.1 (Berkeley) 2/5/92
+ */
+
+/*
+ * Machine dependent trace commands.
+ *
+ * None for the i386 at this time.
+ */
