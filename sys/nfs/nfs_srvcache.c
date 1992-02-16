ff1554c2b0411e0c63066753da2278c378bf6834
Kirk McKusick <mckusick>
1992-02-15 17:37:58 -0800

    have to include proc before socketvar.h

--- sys/nfs/nfs_srvcache.c
+++ sys/nfs/nfs_srvcache.c
@@ -7,7 +7,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)nfs_srvcache.c	7.12 (Berkeley) 1/14/92
+ *	@(#)nfs_srvcache.c	7.13 (Berkeley) 2/15/92
  */
 
 /*
@@ -20,6 +20,7 @@
 #include "mount.h"
 #include "kernel.h"
 #include "systm.h"
+#include "proc.h"
 #include "mbuf.h"
 #include "socket.h"
 #include "socketvar.h"
