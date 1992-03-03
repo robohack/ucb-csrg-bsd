3a8a1ed1fa2f7c2b0ae55c0dc09902a165b89295
Keith Sklower <sklower>
1992-03-02 16:25:29 -0800

    can use #include "param.h" instead of #include "sys/param.h"; move file to /sys/netns from /sys/tahoe/tahoe (this version is machine- independent and not horribly inefficient

--- sys/netns/ns_cksum.c
+++ sys/netns/ns_cksum.c
@@ -4,11 +4,11 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)ns_cksum.c	7.7 (Berkeley) 4/29/91
+ *	@(#)ns_cksum.c	7.8 (Berkeley) 3/2/92
  */
 
-#include "sys/param.h"
-#include "sys/mbuf.h"
+#include "param.h"
+#include "mbuf.h"
 
 /*
  * Checksum routine for Network Systems Protocol Packets (Big-Endian).
