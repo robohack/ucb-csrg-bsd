c81af7aa31da2e4a2b78b861e249808138a5ec53
Keith Bostic <bostic>
1992-03-02 17:22:34 -0800

    declare struct proc, struct uio before using in prototypes

--- sys/sys/file.h
+++ sys/sys/file.h
@@ -4,13 +4,16 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)file.h	7.10 (Berkeley) 6/4/91
+ *	@(#)file.h	7.11 (Berkeley) 3/2/92
  */
 
 #include <sys/fcntl.h>
 #include <sys/unistd.h>
 
 #ifdef KERNEL
+struct proc;
+struct uio;
+
 /*
  * Kernel descriptor table.
  * One entry for each open kernel vnode and socket.
