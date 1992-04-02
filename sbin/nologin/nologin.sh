a0c9621f64d329b960a4fd68279d2bd75c44df4b
Keith Bostic <bostic>
1992-04-02 14:05:25 -0800

    date and time created 92/04/02 14:05:25 by bostic

---
+++ sbin/nologin/nologin.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -
+#
+# Copyright (c) 1992 The Regents of the University of California.
+# All rights reserved.
+#
+# %sccs.include.redist.sh%
+#
+#	@(#)nologin.sh	5.1 (Berkeley) 4/2/92
+#
+
+echo 'This account is currently not available.'
+exit 1
