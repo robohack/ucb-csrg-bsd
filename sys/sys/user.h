2d7efd4e4b85591f3d241012e9e661aeedc60c80
Kirk McKusick <mckusick>
1992-06-19 22:28:26 -0800

    add md_coredump

--- sys/sys/user.h
+++ sys/sys/user.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)user.h	7.20 (Berkeley) 2/5/92
+ *	@(#)user.h	7.21 (Berkeley) 6/19/92
  */
 
 #include <machine/pcb.h>
@@ -40,6 +40,7 @@ struct	user {
 	 * not valid at other times!
 	 */
 	struct	kinfo_proc u_kproc;	/* proc + eproc */
+	struct	md_coredump u_md;	/* machine dependent glop */
 };
 
 /*
