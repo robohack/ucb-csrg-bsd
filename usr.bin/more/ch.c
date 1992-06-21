3d081d8f2818d835c8556a53c28644d21ffd27bf
Keith Bostic <bostic>
1992-06-21 14:42:27 -0800

    off_t is a quad, now

--- usr.bin/more/ch.c
+++ usr.bin/more/ch.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)ch.c	5.10 (Berkeley) 3/1/91";
+static char sccsid[] = "@(#)ch.c	5.11 (Berkeley) 6/21/92";
 #endif /* not lint */
 
 /*
@@ -18,6 +18,7 @@ static char sccsid[] = "@(#)ch.c	5.10 (Berkeley) 3/1/91";
 
 #include <sys/types.h>
 #include <sys/file.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <less.h>
 
