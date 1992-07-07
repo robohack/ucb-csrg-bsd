c413380061e3e78366b08ae218a89cd3fbc1ceba
Keith Sklower <sklower>
1992-07-07 15:57:15 -0800

    not every value return in an nlist structure was being coerced to an (off_t) which was only being smashed back down to a (u_long) as an argument to kvm_read.  If you ask me nl.n_value ought to be a (void *) but since the type of nl.n_value and the second argument to kvm_read are the same I got fed up and changed all the off_t's to be that type (u_long).

--- usr.bin/netstat/mbuf.c
+++ usr.bin/netstat/mbuf.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)mbuf.c	5.11 (Berkeley) 5/27/92";
+static char sccsid[] = "@(#)mbuf.c	5.12 (Berkeley) 7/7/92";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -51,7 +51,7 @@ bool seen[256];			/* "have we seen this type yet?" */
  */
 void
 mbpr(mbaddr)
-	off_t mbaddr;
+	u_long mbaddr;
 {
 	register int totmem, totfree, totmbufs;
 	register int i;
