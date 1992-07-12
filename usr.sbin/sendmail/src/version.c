3f1b67cf534703bbd2faf249240ab01ba115ccc1
Eric Allman <eric>
1992-07-12 06:26:49 -0800

    many fixes to controlling user code; fix Neil Rickert's hack using CNAME for someone else's host

--- usr.sbin/sendmail/src/version.c
+++ usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	5.101 (Berkeley) 7/11/92";
+static char sccsid[] = "@(#)version.c	5.102 (Berkeley) 7/12/92";
 #endif /* not lint */
 
-char	Version[] = "5.101";
+char	Version[] = "5.102";
