a12c55aa8799e0e6d8a9d9fe00582a92b491e3ec
Keith Bostic <bostic>
1992-07-21 09:29:34 -0800

    date and time created 92/07/21 10:29:34 by bostic

---
+++ usr.sbin/lpr/lpd/extern.h
@@ -0,0 +1,11 @@
+/*-
+ * Copyright (c) 1992 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)extern.h	5.1 (Berkeley) 7/21/92
+ */
+
+void       printjob __P((void));
+int        recvjob __P((void));
