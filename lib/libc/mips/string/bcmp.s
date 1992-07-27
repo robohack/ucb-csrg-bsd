91c53deeaf88938fa9d3a2be4ab0e4da1e81a30a
Ralph Campbell <ralph>
1992-07-26 20:41:33 -0800

    removed DEFS.h

--- lib/libc/mips/string/bcmp.s
+++ lib/libc/mips/string/bcmp.s
@@ -8,10 +8,10 @@
  * %sccs.include.redist.c%
  */
 
-#include "DEFS.h"
+#include <machine/machAsmDefs.h>
 
 #if defined(LIBC_SCCS) && !defined(lint)
-	ASMSTR("@(#)bcmp.s	5.3 (Berkeley) 6/20/92")
+	ASMSTR("@(#)bcmp.s	5.4 (Berkeley) 7/26/92")
 #endif /* LIBC_SCCS and not lint */
 
 /* bcmp(s1, s2, n) */
