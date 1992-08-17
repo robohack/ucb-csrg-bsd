f6d6109ad59a1838bf72c1f49dfbf0108fe26751
Edward Wang <edward>
1992-08-16 16:43:30 -0800

    changes from Brian Buhrow, primitive yank and put, Posix MIN and TIME, and others

--- usr.bin/window/main.c
+++ usr.bin/window/main.c
@@ -9,7 +9,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	3.42 (Berkeley) 8/12/90";
+static char sccsid[] = "@(#)main.c	3.43 (Berkeley) 8/16/92";
 #endif /* not lint */
 
 #include "defs.h"
@@ -158,7 +158,7 @@ char **argv;
 	mloop();
 
 bad:
-	wwend();
+	wwend(1);
 	return 0;
 }
 
