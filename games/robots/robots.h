086d0a243d82b233251815883024414ebc37e063
Elan Amir <elan>
1992-08-31 09:16:47 -0800

    Deleted CTRL definition.

--- games/robots/robots.h
+++ games/robots/robots.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)robots.h	5.6 (Berkeley) 2/28/91
+ *	@(#)robots.h	5.7 (Berkeley) 8/31/92
  */
 
 # include	<curses.h>
@@ -37,13 +37,6 @@
 # define	ROBOT	'+'
 # define	HEAP	'*'
 # define	PLAYER	'@'
-
-/*
- * pseudo functions
- */
-
-# undef		CTRL
-# define	CTRL(X)	('X' - 'A' + 1)
 
 /*
  * type definitions
@@ -82,3 +75,7 @@ int	cmp_sc();
 void	move_robots();
 
 COORD	*rnd_pos();
+
+
+
+
