61d71c5d786db1538952904106cd4f267de5f8d0
Kirk McKusick <mckusick>
1992-09-21 18:06:01 -0800

    need to include systm.h to get min and max

--- sys/netiso/iso.c
+++ sys/netiso/iso.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)iso.c	7.18 (Berkeley) 7/9/92
+ *	@(#)iso.c	7.19 (Berkeley) 9/21/92
  */
 
 /***********************************************************
@@ -40,8 +40,8 @@ SOFTWARE.
  * iso.c: miscellaneous routines to support the iso address family
  */
 
-#include "types.h"
 #include "param.h"
+#include "systm.h"
 #include "ioctl.h"
 #include "mbuf.h"
 #include "domain.h"
--- sys/netiso/tp_subr.c
+++ sys/netiso/tp_subr.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)tp_subr.c	7.18 (Berkeley) 5/27/92
+ *	@(#)tp_subr.c	7.19 (Berkeley) 9/21/92
  */
 
 /***********************************************************
@@ -49,6 +49,7 @@ SOFTWARE.
  */
 
 #include "param.h"
+#include "systm.h"
 #include "mbuf.h"
 #include "socket.h"
 #include "socketvar.h"
