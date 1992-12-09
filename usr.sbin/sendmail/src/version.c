e58660e104efcd72359c6610bf1fa44dd697e12d
Eric Allman <eric>
1992-12-09 09:24:10 -0800

    reverse sense of -f in K field

--- usr.sbin/sendmail/src/version.c
+++ usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	5.127 (Berkeley) 12/8/92";
+static char sccsid[] = "@(#)version.c	5.128 (Berkeley) 12/9/92";
 #endif /* not lint */
 
-char	Version[] = "5.127";
+char	Version[] = "5.128";
