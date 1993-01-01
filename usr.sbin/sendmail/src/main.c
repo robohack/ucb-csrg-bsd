b05788c412e3a6bb527a4f97616a8cf4cdd49b90
Eric Allman <eric>
1993-01-01 13:30:03 -0800

    fix botch in invalidaddr() routine

--- usr.sbin/sendmail/src/main.c
+++ usr.sbin/sendmail/src/main.c
@@ -13,7 +13,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	6.2 (Berkeley) 1/1/93";
+static char sccsid[] = "@(#)main.c	6.3 (Berkeley) 1/1/93";
 #endif /* not lint */
 
 #define	_DEFINE
@@ -691,10 +691,7 @@ main(argc, argv, envp)
 			}
 			*p = '\0';
 			if (invalidaddr(p + 1))
-			{
-				printf("Invalid control characters in address\n");
 				continue;
-			}
 			do
 			{
 				extern char **prescan();
