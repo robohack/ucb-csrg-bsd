aeaeb91950e3e32c6007cea59b3625bb6f352eec
Eric Allman <eric>
1993-01-03 12:08:20 -0800

    fix botch causing queued headers to have ?XX? prefixes

--- usr.sbin/sendmail/src/version.c
+++ usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	6.4 (Berkeley) 1/2/93";
+static char sccsid[] = "@(#)version.c	6.5 (Berkeley) 1/3/93";
 #endif /* not lint */
 
-char	Version[] = "ALPHA-6.4";
+char	Version[] = "ALPHA-6.5";
