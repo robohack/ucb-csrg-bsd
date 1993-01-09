77758463f0b42543f9bd7e5785dbeadff62210bd
Keith Bostic <bostic>
1993-01-09 10:34:03 -0800

    seteuid fix from Chris Torek

--- usr.bin/rdist/docmd.c
+++ usr.bin/rdist/docmd.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)docmd.c	5.11 (Berkeley) 7/9/92";
+static char sccsid[] = "@(#)docmd.c	5.12 (Berkeley) 1/9/93";
 #endif /* not lint */
 
 #include "defs.h"
@@ -223,7 +223,7 @@ makeconn(rhost)
 	fflush(stdout);
 	seteuid(0);
 	rem = rcmd(&rhost, port, user, ruser, buf, 0);
-	setreuid(0, userid);
+	seteuid(userid);
 	if (rem < 0)
 		return(0);
 	cp = buf;
