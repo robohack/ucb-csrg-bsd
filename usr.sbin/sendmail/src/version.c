7630b3e8646bf28ed0c7d85b5a735a9ad5f71b07
Eric Allman <eric>
1993-02-15 12:00:22 -0800

    improve some error messages; fix quoted :include:s; check for some other buffer overflows; log SMTP protocol errors

--- usr.sbin/sendmail/src/version.c
+++ usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	6.20 (Berkeley) 2/14/93";
+static char sccsid[] = "@(#)version.c	6.21 (Berkeley) 2/15/93";
 #endif /* not lint */
 
-char	Version[] = "ALPHA-6.20";
+char	Version[] = "ALPHA-6.21";
