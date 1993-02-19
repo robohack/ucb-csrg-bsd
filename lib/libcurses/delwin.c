b4989701b9ade683ef3bf627a70ee0d66da158c9
Elan Amir <elan>
1993-02-18 16:38:14 -0800

    Got rid of unused variable i.

--- lib/libcurses/delwin.c
+++ lib/libcurses/delwin.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)delwin.c	5.10 (Berkeley) 1/11/93";
+static char sccsid[] = "@(#)delwin.c	5.11 (Berkeley) 2/18/93";
 #endif	/* not lint */
 
 #include <curses.h>
@@ -22,7 +22,6 @@ delwin(win)
 {
 
 	register WINDOW *wp, *np;
-	register int i;
 
 	if (win->orig == NULL) {
 		/*
