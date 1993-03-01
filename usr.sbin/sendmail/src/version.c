925f13bb0beb5bdd6df39d92e777fd2f48e161a5
Eric Allman <eric>
1993-02-28 20:00:13 -0800

    fix another owner- bug; improve logging; run all programs without ctladdrs as daemon; add -qXstring to limit queue run; hooks for ESMTP ; improve syntax checking in readcf

--- usr.sbin/sendmail/src/version.c
+++ usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	6.30 (Berkeley) 2/27/93";
+static char sccsid[] = "@(#)version.c	6.31 (Berkeley) 2/28/93";
 #endif /* not lint */
 
-char	Version[] = "ALPHA-6.30";
+char	Version[] = "ALPHA-6.31";
