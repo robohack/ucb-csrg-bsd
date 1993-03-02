0b7c825c368db92708558eb04fda3f1d57dd792c
Eric Allman <eric>
1993-03-02 14:27:53 -0800

    fix some uninitialized variables; <unistd.h> everywhere; logging changes; accept host name inside [...] to avoid MX lookup

--- usr.sbin/sendmail/src/version.c
+++ usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	6.31 (Berkeley) 2/28/93";
+static char sccsid[] = "@(#)version.c	6.32 (Berkeley) 3/2/93";
 #endif /* not lint */
 
-char	Version[] = "ALPHA-6.31";
+char	Version[] = "ALPHA-6.32";
