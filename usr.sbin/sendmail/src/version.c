a98994b21f994566564e594fa1e5a260c06189f6
Eric Allman <eric>
1993-03-05 15:09:43 -0800

    avoid loops on nameserver NO_DATA errors; getsockname can return zero length names on SunOS

--- usr.sbin/sendmail/src/version.c
+++ usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	6.34 (Berkeley) 3/5/93";
+static char sccsid[] = "@(#)version.c	6.35 (Berkeley) 3/5/93";
 #endif /* not lint */
 
-char	Version[] = "ALPHA-6.34";
+char	Version[] = "ALPHA-6.35";
