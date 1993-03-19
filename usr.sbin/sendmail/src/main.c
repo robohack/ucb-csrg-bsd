1e9e9e5538ac40bbd6db037da7660d1ebba53d52
Eric Allman <eric>
1993-03-18 19:37:50 -0800

    avoid extra unnecessary creation of queue files solely for an unneeded id

--- usr.sbin/sendmail/src/main.c
+++ usr.sbin/sendmail/src/main.c
@@ -13,7 +13,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	6.41 (Berkeley) 3/18/93";
+static char sccsid[] = "@(#)main.c	6.42 (Berkeley) 3/18/93";
 #endif /* not lint */
 
 #define	_DEFINE
@@ -871,7 +871,6 @@ main(argc, argv, envp)
 		/* at this point we are in a child: reset state */
 		OpMode = MD_SMTP;
 		(void) newenvelope(CurEnv, CurEnv);
-		openxscript(CurEnv);
 #endif /* DAEMON */
 	}
 	
