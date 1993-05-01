318372121f599bb64fed6091f678001c9f34a55c
Eric Allman <eric>
1993-05-01 08:07:13 -0800

    fix Postmaster alias in fallback mail return; fix flock problem for /dev/null; add "b" and "g" mailer flags; don't change case on header field names; improved error message reporting for queue conditions; HPUX tweaking; misc fixes

--- usr.sbin/sendmail/src/version.c
+++ usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	6.55 (Berkeley) 4/27/93";
+static char sccsid[] = "@(#)version.c	6.56 (Berkeley) 5/1/93";
 #endif /* not lint */
 
-char	Version[] = "ALPHA-6.55";
+char	Version[] = "ALPHA-6.56";
