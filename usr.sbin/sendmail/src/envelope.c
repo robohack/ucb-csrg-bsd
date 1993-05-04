bfd57a1f486a0e0de5b78844869abd103ef59825
Eric Allman <eric>
1993-05-03 16:21:52 -0800

    if we are in Verbose mode, be sure to set sendmode to SM_DELIVER

--- usr.sbin/sendmail/src/envelope.c
+++ usr.sbin/sendmail/src/envelope.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)envelope.c	6.33 (Berkeley) 4/27/93";
+static char sccsid[] = "@(#)envelope.c	6.34 (Berkeley) 5/3/93";
 #endif /* not lint */
 
 #include "sendmail.h"
@@ -199,6 +199,8 @@ clearenvelope(e, fullclear)
 
 	/* now clear out the data */
 	STRUCTCOPY(BlankEnvelope, *e);
+	if (Verbose)
+		e->e_sendmode = SM_DELIVER;
 	bh = BlankEnvelope.e_header;
 	nhp = &e->e_header;
 	while (bh != NULL)
