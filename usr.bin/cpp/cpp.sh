8dca8dea16648bf701b4cd6447eddfb416cc53b8
Keith Bostic <bostic>
1993-05-03 20:10:31 -0800

    gcc2; cpp now in /usr/libexec/gcc2/cpp

--- usr.bin/cpp/cpp.sh
+++ usr.bin/cpp/cpp.sh
@@ -9,14 +9,14 @@
 #
 # %sccs.include.redist.sh%
 #
-#	@(#)cpp.sh	6.5 (Berkeley) 4/1/91
+#	@(#)cpp.sh	6.6 (Berkeley) 5/3/93
 #
 # Transitional front end to CCCP to make it behave like (Reiser) CCP:
 #	specifies -traditional
 #	doesn't search gcc-include
 #
 PATH=/usr/bin:/bin
-CPP=/usr/libexec/cpp
+CPP=/usr/libexec/gcc2/cpp
 ALST="-traditional -D__GNUC__ -$ "
 NSI=no
 OPTS=""
