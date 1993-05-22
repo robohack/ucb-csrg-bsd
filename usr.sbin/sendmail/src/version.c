3dc0808da201e880da8d5924ab4e45a1b2f10a10
Eric Allman <eric>
1993-05-22 08:42:50 -0800

    merge alias database with maps, expanding and changing interface; announce mailer/host/user triple on -bv flag; don't send unsolicited multiline HELO replies -- it breaks Pony Express; AIX portability changes

--- usr.sbin/sendmail/src/version.c
+++ usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	6.59 (Berkeley) 5/13/93";
+static char sccsid[] = "@(#)version.c	6.60 (Berkeley) 5/22/93";
 #endif /* not lint */
 
-char	Version[] = "ALPHA-6.59";
+char	Version[] = "ALPHA-6.60";
