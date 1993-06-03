d5dc4a3332f101f092e683d5d3da36777cd11ee4
Kirk McKusick <mckusick>
1993-06-02 18:06:33 -0800

    devname moves to libc

--- usr.bin/fstat/fstat.c
+++ usr.bin/fstat/fstat.c
@@ -12,7 +12,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)fstat.c	5.45 (Berkeley) 5/25/93";
+static char sccsid[] = "@(#)fstat.c	5.46 (Berkeley) 6/2/93";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -341,7 +341,6 @@ vtrans(vp, i, flag)
 	int i;
 	int flag;
 {
-	extern char *devname();
 	struct vnode vn;
 	struct filestat fst;
 	char rw[3], mode[15];
