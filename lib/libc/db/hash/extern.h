e07ca30526d78d85690a922eff80aad1b58181ef
Keith Bostic <bostic>
1993-06-04 14:21:38 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- lib/libc/db/hash/extern.h
+++ lib/libc/db/hash/extern.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)extern.h	5.3 (Berkeley) 1/17/93
+ *	@(#)extern.h	8.1 (Berkeley) 6/4/93
  */
 
 BUFHEAD	*__add_ovflpage __P((HTAB *, BUFHEAD *));
