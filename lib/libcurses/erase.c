c26ebc398038d0b04077a6284d6a0d5020516f30
Keith Bostic <bostic>
1993-06-04 15:47:56 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- lib/libcurses/erase.c
+++ lib/libcurses/erase.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1981 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1981, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)erase.c	5.12 (Berkeley) 5/16/93";
+static char sccsid[] = "@(#)erase.c	8.1 (Berkeley) 6/4/93";
 #endif	/* not lint */
 
 #include <curses.h>
