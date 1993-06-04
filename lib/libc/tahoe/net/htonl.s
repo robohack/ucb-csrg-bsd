e3eb4f8f0a6d74caa30ac62b6ab4e08c5f9592fc
Keith Bostic <bostic>
1993-06-04 12:16:47 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- lib/libc/tahoe/net/htonl.s
+++ lib/libc/tahoe/net/htonl.s
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1988 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-	.asciz "@(#)htonl.s	5.1 (Berkeley) 4/12/91"
+	.asciz "@(#)htonl.s	8.1 (Berkeley) 6/4/93"
 #endif /* LIBC_SCCS and not lint */
 
 /* netorder = htonl(hostorder) */
--- lib/libc/tahoe/net/htons.s
+++ lib/libc/tahoe/net/htons.s
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1988 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-	.asciz "@(#)htons.s	5.1 (Berkeley) 4/12/91"
+	.asciz "@(#)htons.s	8.1 (Berkeley) 6/4/93"
 #endif /* LIBC_SCCS and not lint */
 
 /* hostorder = htons(netorder) */
