c9617612e328e4448f5e610c6979e0c3fbbe7624
Keith Bostic <bostic>
1993-06-04 15:57:54 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- lib/libm/ieee/cabs.c
+++ lib/libm/ieee/cabs.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1985 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1985, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)cabs.c	5.7 (Berkeley) 3/8/93";
+static char sccsid[] = "@(#)cabs.c	8.1 (Berkeley) 6/4/93";
 #endif /* not lint */
 
 /* HYPOT(X,Y)
