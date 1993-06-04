9582b618adca4686fcb3ce0858f289248c4faf3e
Keith Bostic <bostic>
1993-06-04 11:47:37 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- lib/libc/mips/SYS.h
+++ lib/libc/mips/SYS.h
@@ -1,13 +1,13 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Ralph Campbell.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)SYS.h	5.3 (Berkeley) 2/4/93
+ *	@(#)SYS.h	8.1 (Berkeley) 6/4/93
  */
 
 #include <sys/syscall.h>
