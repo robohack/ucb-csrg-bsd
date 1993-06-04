136db993b4bff1b3b8b97ca3aec201fffeb88a3b
Marc Teitelbaum <marc>
1993-06-04 15:11:55 -0800

    changed to remember and restore TSTP handler, and also to resnapshot tty state when restarting from TSTP.  Also, hardwire GT variable since we always turn tab expansion off.  We still aren't sure what pfast is for, but we haven't given up yet.

--- lib/libcurses/initscr.c
+++ lib/libcurses/initscr.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)initscr.c	5.15 (Berkeley) 5/16/93";
+static char sccsid[] = "@(#)initscr.c	5.16 (Berkeley) 6/4/93";
 #endif	/* not lint */
 
 #include <curses.h>
@@ -57,7 +57,7 @@ initscr()
 		return (NULL);
 	}
 
-	(void)signal(SIGTSTP, __stop_signal_handler);
+	__set_stophandler();
 
 #ifdef DEBUG
 	__CTRACE("initscr: LINES = %d, COLS = %d\n", LINES, COLS);
