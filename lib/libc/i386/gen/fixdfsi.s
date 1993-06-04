f5faa7a1fc5cd5aa0741dcf6b7a6a89ac672af7d
Keith Bostic <bostic>
1993-06-04 11:38:51 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- lib/libc/i386/gen/fixdfsi.s
+++ lib/libc/i386/gen/fixdfsi.s
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * William Jolitz.
@@ -9,7 +9,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-	.asciz "@(#)fixdfsi.s	5.4 (Berkeley) 4/12/91"
+	.asciz "@(#)fixdfsi.s	8.1 (Berkeley) 6/4/93"
 #endif /* LIBC_SCCS and not lint */
 
 	.globl ___fixdfsi
