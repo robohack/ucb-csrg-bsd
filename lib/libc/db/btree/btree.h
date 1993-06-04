8f3a7a4ebfbc0747d9fc1628d58ea574fcd12666
Keith Bostic <bostic>
1993-06-04 14:21:12 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- lib/libc/db/btree/btree.h
+++ lib/libc/db/btree/btree.h
@@ -1,13 +1,13 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Mike Olson.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)btree.h	5.15 (Berkeley) 5/22/93
+ *	@(#)btree.h	8.1 (Berkeley) 6/4/93
  */
 
 #include <mpool.h>
