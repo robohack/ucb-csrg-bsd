c07bafdbaba117a5d8b2bf5a171aa730d2b6cfb8
Keith Bostic <bostic>
1993-06-04 15:48:38 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- lib/libcurses/printw.c
+++ lib/libcurses/printw.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1981 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1981, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)printw.c	5.15 (Berkeley) 2/2/93";
+static char sccsid[] = "@(#)printw.c	8.1 (Berkeley) 6/4/93";
 #endif	/* not lint */
 
 #include <curses.h>
