525e64066731edf21dd5fcb7195bc0899676ab55
Keith Bostic <bostic>
1993-06-04 18:06:48 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- libexec/telnetd/defs.h
+++ libexec/telnetd/defs.h
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 1989 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)defs.h	5.12 (Berkeley) 5/20/93
+ *	@(#)defs.h	8.1 (Berkeley) 6/4/93
  */
 
 /*
