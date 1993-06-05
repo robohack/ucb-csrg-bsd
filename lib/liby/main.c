cc573a50c5d5af498cfffdda0da392e5c0eda4aa
Keith Bostic <bostic>
1993-06-04 16:19:27 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- lib/liby/main.c
+++ lib/liby/main.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	5.3 (Berkeley) 1/13/91";
+static char sccsid[] = "@(#)main.c	8.1 (Berkeley) 6/4/93";
 #endif /* not lint */
 
 main()
