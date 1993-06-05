9219cfa30ee271068504565884093748b0c05e28
Keith Bostic <bostic>
1993-06-04 17:50:09 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- libexec/getty/extern.h
+++ libexec/getty/extern.h
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 1993 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)extern.h	5.2 (Berkeley) 5/27/93
+ *	@(#)extern.h	8.1 (Berkeley) 6/4/93
  */
 
 struct delayval;
--- libexec/getty/gettytab.h
+++ libexec/getty/gettytab.h
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 1983 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)gettytab.h	5.6 (Berkeley) 5/17/93
+ *	@(#)gettytab.h	8.1 (Berkeley) 6/4/93
  */
 
 /*
