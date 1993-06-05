2343026a4f4d2b42b53a18b63f1a756828d6adf3
Keith Bostic <bostic>
1993-06-04 17:18:14 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- lib/libplot/t450/space.c
+++ lib/libplot/t450/space.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1983 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.proprietary.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)space.c	4.2 (Berkeley) 4/22/91";
+static char sccsid[] = "@(#)space.c	8.1 (Berkeley) 6/4/93";
 #endif /* not lint */
 
 # include "con.h"
