b4e67f0472d396d636d51f4189aeeea84095bf47
Keith Bostic <bostic>
1993-06-05 09:46:20 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- sbin/XNSrouted/defs.h
+++ sbin/XNSrouted/defs.h
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 1983 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)defs.h	5.10 (Berkeley) 2/20/92
+ *	@(#)defs.h	8.1 (Berkeley) 6/5/93
  */
 
 #include <sys/types.h>
