028eb0b112417d340dd4cdaf939c685e29b7a584
Keith Bostic <bostic>
1993-06-04 16:50:14 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- lib/libplot/t4013/box.c
+++ lib/libplot/t4013/box.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1985 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1985, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.proprietary.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)box.c	5.2 (Berkeley) 4/22/91";
+static char sccsid[] = "@(#)box.c	8.1 (Berkeley) 6/4/93";
 #endif /* not lint */
 
 box(x0, y0, x1, y1)
