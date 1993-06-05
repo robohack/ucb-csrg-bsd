5a701458dff5076f7b757b7f4e644eb5fb37940a
Keith Bostic <bostic>
1993-06-05 10:11:40 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- sbin/restore/dirs.c
+++ sbin/restore/dirs.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1983 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)dirs.c	5.27 (Berkeley) 2/11/93";
+static char sccsid[] = "@(#)dirs.c	8.1 (Berkeley) 6/5/93";
 #endif /* not lint */
 
 #include <sys/param.h>
