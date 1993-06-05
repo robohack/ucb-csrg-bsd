60eaca7986b97d985d0a750cc62a444fc58b89a0
Keith Bostic <bostic>
1993-06-04 17:55:37 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- libexec/lfs_cleanerd/print.c
+++ libexec/lfs_cleanerd/print.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)print.c	5.2 (Berkeley) 8/21/92";
+static char sccsid[] = "@(#)print.c	8.1 (Berkeley) 6/4/93";
 #endif /* not lint */
 
 #include <sys/param.h>
