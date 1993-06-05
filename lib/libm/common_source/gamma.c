cba59a953812f2d325f07e52387422fab7975563
Keith Bostic <bostic>
1993-06-04 16:02:18 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- lib/libm/common_source/gamma.c
+++ lib/libm/common_source/gamma.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)gamma.c	5.4 (Berkeley) 1/10/93";
+static char sccsid[] = "@(#)gamma.c	8.1 (Berkeley) 6/4/93";
 #endif /* not lint */
 
 /*
