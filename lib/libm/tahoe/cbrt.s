ce25ddd8b6ca4178e9741d8bd586d6943e14baaa
Keith Bostic <bostic>
1993-06-04 16:08:27 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- lib/libm/tahoe/cbrt.s
+++ lib/libm/tahoe/cbrt.s
@@ -1,14 +1,14 @@
-# Copyright (c) 1987 Regents of the University of California.
-# All rights reserved.
+# Copyright (c) 1987, 1993
+#	The Regents of the University of California.  All rights reserved.
 #
 # %sccs.include.redist.sh%
 #
-#	@(#)cbrt.s	5.5 (Berkeley) 10/9/90
+#	@(#)cbrt.s	8.1 (Berkeley) 6/4/93
 #
 	.data
 	.align	2
 _sccsid:
-.asciz	"@(#)cbrt.s	5.5	(ucb.elefunt) 10/9/90"
+.asciz	"@(#)cbrt.s	8.1	(ucb.elefunt) 6/4/93"
 
 # double cbrt(double arg)
 # W. Kahan, 10/13/80. revised 1/13/84 for keeping sign symmetry
