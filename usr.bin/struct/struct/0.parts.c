556e392970f05aea103ffaa9274dfe9b35703e83
Keith Bostic <bostic>
1993-06-06 15:28:28 -0800

    4.4BSD snapshot (revision 8.1)

--- usr.bin/struct/struct/0.parts.c
+++ usr.bin/struct/struct/0.parts.c
@@ -3,7 +3,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)0.parts.c	4.2 (Berkeley) 4/16/91";
+static char sccsid[] = "@(#)0.parts.c	8.1 (Berkeley) 6/6/93";
 #endif /* not lint */
 
 #include <stdio.h>
