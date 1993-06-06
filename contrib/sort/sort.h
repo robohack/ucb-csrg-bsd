9bf3ee388b8211201fff7a96a4fc710ddcd4da94
Keith Bostic <bostic>
1993-06-06 15:23:14 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- contrib/sort/sort.h
+++ contrib/sort/sort.h
@@ -1,13 +1,13 @@
 /*-
- * Copyright (c) 1993 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Peter McIlroy.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)sort.h	5.1 (Berkeley) 6/1/93
+ *	@(#)sort.h	8.1 (Berkeley) 6/6/93
  */
 
 #include <sys/param.h>
