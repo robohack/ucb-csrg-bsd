778c3b83f07f00089546232f9b18941216a2f63b
Keith Bostic <bostic>
1993-06-06 15:24:19 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.bin/spell/spell.h
+++ usr.bin/spell/spell.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.proprietary.c%
  *
- *	@(#)spell.h	4.3 (Berkeley) 4/18/91
+ *	@(#)spell.h	8.1 (Berkeley) 6/6/93
  */
 
 #include <stdio.h>
