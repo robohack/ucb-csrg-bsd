75129f6155bac5a0d9ad67c25a9d1d231ebcd748
Keith Bostic <bostic>
1993-06-06 14:18:27 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.bin/pascal/libpc/RELNE.c
+++ usr.bin/pascal/libpc/RELNE.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1979 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1979, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)RELNE.c	1.3 (Berkeley) 4/9/90";
+static char sccsid[] = "@(#)RELNE.c	8.1 (Berkeley) 6/6/93";
 #endif /* not lint */
 
 #include "h00vars.h"
--- usr.bin/pascal/libpc/SQRT.c
+++ usr.bin/pascal/libpc/SQRT.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1979 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1979, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)SQRT.c	1.5 (Berkeley) 4/9/90";
+static char sccsid[] = "@(#)SQRT.c	8.1 (Berkeley) 6/6/93";
 #endif /* not lint */
 
 #include <math.h>
