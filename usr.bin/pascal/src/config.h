6ff4d7362c19c7f82dead45a584504da396e6e60
Keith Bostic <bostic>
1993-06-06 15:07:59 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.bin/pascal/src/config.h
+++ usr.bin/pascal/src/config.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1980 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)config.h	5.4 (Berkeley) 4/16/91
+ *	@(#)config.h	8.1 (Berkeley) 6/6/93
  */
 
     /*
