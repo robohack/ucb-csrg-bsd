10894a7a17f5da84b9ace33c99b47c11adcd6e9d
Keith Bostic <bostic>
1993-06-06 13:37:29 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.bin/diff/diff3/diff3.sh
+++ usr.bin/diff/diff3/diff3.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -
 #
-# Copyright (c) 1991 The Regents of the University of California.
-# All rights reserved.
+# Copyright (c) 1991, 1993
+#	The Regents of the University of California.  All rights reserved.
 #
 # %sccs.include.proprietary.sh%
 #
-#	@(#)diff3.sh	4.3 (Berkeley) 4/17/91
+#	@(#)diff3.sh	8.1 (Berkeley) 6/6/93
 #
 
 e=
