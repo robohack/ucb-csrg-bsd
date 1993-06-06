ca645664fcf4382605f0ca933cd34c3c01733648
Keith Bostic <bostic>
1993-06-06 14:35:19 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.bin/more/output.c
+++ usr.bin/more/output.c
@@ -1,13 +1,13 @@
 /*
  * Copyright (c) 1988 Mark Nudleman
- * Copyright (c) 1988 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)output.c	5.10 (Berkeley) 7/24/91";
+static char sccsid[] = "@(#)output.c	8.1 (Berkeley) 6/6/93";
 #endif /* not lint */
 
 /*
