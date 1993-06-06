e96e4f62cd69f31a2b409f4545b1aa952bc5885d
Keith Bostic <bostic>
1993-06-06 13:38:39 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.bin/finger/net.c
+++ usr.bin/finger/net.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1989 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Tony Nardo of the Johns Hopkins University/Applied Physics Lab.
@@ -9,7 +9,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)net.c	5.7 (Berkeley) 7/27/91";
+static char sccsid[] = "@(#)net.c	8.1 (Berkeley) 6/6/93";
 #endif /* not lint */
 
 #include <sys/types.h>
