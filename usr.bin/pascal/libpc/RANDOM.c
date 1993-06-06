68cdd8cde41883d17ca3c6b8aedd616e0e6dc8a2
Keith Bostic <bostic>
1993-06-06 14:17:54 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.bin/pascal/libpc/RANDOM.c
+++ usr.bin/pascal/libpc/RANDOM.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1979 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1979, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)RANDOM.c	1.5 (Berkeley) 4/9/90";
+static char sccsid[] = "@(#)RANDOM.c	8.1 (Berkeley) 6/6/93";
 #endif /* not lint */
 
 #include "h00vars.h"
