242464674941e8d038065c06fbed52f98bc512de
Keith Bostic <bostic>
1993-06-06 15:22:40 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- contrib/sort/extern.h
+++ contrib/sort/extern.h
@@ -1,13 +1,13 @@
 /*-
- * Copyright (c) 1993 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Peter McIlroy.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)extern.h	5.1 (Berkeley) 6/1/93
+ *	@(#)extern.h	8.1 (Berkeley) 6/6/93
  */
 
 void	 append  __P((u_char **, int, int, FILE *, void (*)(), struct field *));
