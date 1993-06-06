66d79e12c2cdf2496369ce3c4bcc282e27333ed5
Keith Bostic <bostic>
1993-06-06 15:31:43 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.bin/talk/msgs.c
+++ usr.bin/talk/msgs.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1983 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)msgs.c	5.6 (Berkeley) 3/1/91";
+static char sccsid[] = "@(#)msgs.c	8.1 (Berkeley) 6/6/93";
 #endif /* not lint */
 
 /* 
