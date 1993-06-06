891475cd9ecc97854e643dc3ad557bbdf97e0081
Keith Bostic <bostic>
1993-06-06 12:59:46 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.sbin/lpr/lpc/cmdtab.c
+++ usr.sbin/lpr/lpc/cmdtab.c
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 1983 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)cmdtab.c	5.6 (Berkeley) 4/5/93";
+static char sccsid[] = "@(#)cmdtab.c	8.1 (Berkeley) 6/6/93";
 #endif /* not lint */
 
 #include <sys/cdefs.h>
