ec2b2eb22ce378360bcd47d72ecbe6c6e926cfe2
Keith Bostic <bostic>
1993-06-06 15:29:21 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.bin/systat/netcmds.c
+++ usr.bin/systat/netcmds.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1980, 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)netcmds.c	5.7 (Berkeley) 5/21/93";
+static char sccsid[] = "@(#)netcmds.c	8.1 (Berkeley) 6/6/93";
 #endif /* not lint */
 
 /*
