a386fa833a6b540f0a920884c08cace56ab3ecb6
Keith Bostic <bostic>
1993-06-06 13:26:59 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.bin/chpass/table.c
+++ usr.bin/chpass/table.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)table.c	5.1 (Berkeley) 2/12/91";
+static char sccsid[] = "@(#)table.c	8.1 (Berkeley) 6/6/93";
 #endif /* not lint */
 
 #include <sys/types.h>
