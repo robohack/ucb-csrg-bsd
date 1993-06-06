1b10618de6342b0f63e5333919e355e36379e3bf
Keith Bostic <bostic>
1993-06-06 15:29:29 -0800

    4.4BSD snapshot (revision 8.1)

--- usr.bin/struct/struct/2.head.c
+++ usr.bin/struct/struct/2.head.c
@@ -3,7 +3,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)2.head.c	4.2 (Berkeley) 4/16/91";
+static char sccsid[] = "@(#)2.head.c	8.1 (Berkeley) 6/6/93";
 #endif /* not lint */
 
 #include <stdio.h>
