e1a166506274d73c5d48f200c2d55224ba330f0b
Keith Bostic <bostic>
1993-06-06 21:19:32 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.bin/vgrind/extern.h
+++ usr.bin/vgrind/extern.h
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 1980 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  *
  * %sccs.include.redist.c%
  *
- *      @(#)extern.h	1.3 (Berkeley) 9/15/92
+ *      @(#)extern.h	8.1 (Berkeley) 6/6/93
  */
 
 typedef int boolean;
