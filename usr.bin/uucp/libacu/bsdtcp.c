87c2e78aaab356c6502102f78eabd3ff847b0700
Keith Bostic <bostic>
1993-06-06 21:08:42 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.bin/uucp/libacu/bsdtcp.c
+++ usr.bin/uucp/libacu/bsdtcp.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1985 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1985, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.proprietary.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)bsdtcp.c	4.7 (Berkeley) 5/17/93";
+static char sccsid[] = "@(#)bsdtcp.c	8.1 (Berkeley) 6/6/93";
 #endif /* not lint */
 
 #include "condevs.h"
--- usr.bin/uucp/libacu/vmacs.c
+++ usr.bin/uucp/libacu/vmacs.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1985 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1985, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.proprietary.c%
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)vmacs.c	4.7 (Berkeley) 4/24/91";
+static char sccsid[] = "@(#)vmacs.c	8.1 (Berkeley) 6/6/93";
 #endif /* not lint */
 
 #include "condevs.h"
