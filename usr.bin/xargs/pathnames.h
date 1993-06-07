875335f4b7db0b32469cf15b66c73a2ec15c6a6b
Keith Bostic <bostic>
1993-06-06 21:28:11 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- usr.bin/xargs/pathnames.h
+++ usr.bin/xargs/pathnames.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)pathnames.h	5.1 (Berkeley) 4/17/90
+ *	@(#)pathnames.h	8.1 (Berkeley) 6/6/93
  */
 
 #define	_PATH_ECHO	"/bin/echo"
