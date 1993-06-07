80e83e5b5c797258afb376d784874ceaa482d28f
Keith Bostic <bostic>
1993-06-06 21:30:14 -0800

    4.4BSD snapshot (revision 8.1)

--- usr.bin/xsend/enroll/enroll.c
+++ usr.bin/xsend/enroll/enroll.c
@@ -3,7 +3,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)enroll.c	4.4 (Berkeley) 4/18/91";
+static char sccsid[] = "@(#)enroll.c	8.1 (Berkeley) 6/6/93";
 #endif /* not lint */
 
 #include <sys/types.h>
