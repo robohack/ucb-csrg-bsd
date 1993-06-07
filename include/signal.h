10ffbaa945d24f2f6ab89b386082f2d9639dc8c1
Keith Bostic <bostic>
1993-06-06 20:57:33 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- include/signal.h
+++ include/signal.h
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)signal.h	5.6 (Berkeley) 5/7/93
+ *	@(#)signal.h	8.1 (Berkeley) 6/6/93
  */
 
 #ifndef _USER_SIGNAL_H
