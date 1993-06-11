c2d6a804682f180baf1a56e13f3d25793ad9d2cd
Keith Bostic <bostic>
1993-06-11 14:30:00 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- sys/i386/floppy/mv/mv.c
+++ sys/i386/floppy/mv/mv.c
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)mv.c	5.2 (Berkeley) 5/9/91
+ *	@(#)mv.c	8.1 (Berkeley) 6/11/93
  */
 
 int eval;
