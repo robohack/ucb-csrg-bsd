6878e3ccca75baaec17d16626abc69c629ac80c1
Keith Bostic <bostic>
1993-06-11 14:29:27 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright

--- sys/i386/floppy/mkdir/mkdir.c
+++ sys/i386/floppy/mkdir/mkdir.c
@@ -1,10 +1,10 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * %sccs.include.redist.c%
  *
- *	@(#)mkdir.c	5.1 (Berkeley) 5/9/91
+ *	@(#)mkdir.c	8.1 (Berkeley) 6/11/93
  */
 
 int eval;
