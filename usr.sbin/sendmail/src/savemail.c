d8ef9549ef2ddf76b8b506b932e9915f5a54d086
Eric Allman <eric>
1993-07-23 13:51:44 -0800

    tag returned message with true original from (not localhost)

--- usr.sbin/sendmail/src/savemail.c
+++ usr.sbin/sendmail/src/savemail.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)savemail.c	8.7 (Berkeley) 7/19/93";
+static char sccsid[] = "@(#)savemail.c	8.8 (Berkeley) 7/23/93";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -571,7 +571,7 @@ errbody(fp, m, e)
 
 	sprintf(buf, "The original message was received at %s", arpadate(NULL));
 	putline(buf, fp, m);
-	expand("from \201_", buf, &buf[sizeof buf - 1], e);
+	expand("from \201_", buf, &buf[sizeof buf - 1], e->e_parent);
 	putline(buf, fp, m);
 	putline("", fp, m);
 
