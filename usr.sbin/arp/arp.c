439668fc5152aadc76b9bb37d969f64d93d17af5
Keith Bostic <bostic>
1994-01-02 12:53:54 -0800

    If you're going to fall through, say so! From: Craig Leres <leres@ee.lbl.gov>

--- usr.sbin/arp/arp.c
+++ usr.sbin/arp/arp.c
@@ -15,7 +15,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)arp.c	8.1 (Berkeley) 6/6/93";
+static char sccsid[] = "@(#)arp.c	8.2 (Berkeley) 1/2/94";
 #endif /* not lint */
 
 /*
@@ -448,6 +448,7 @@ rtmsg(cmd)
 				rtm->rtm_flags &= ~RTF_HOST;
 			}
 		}
+		/* FALLTHROUGH */
 	case RTM_GET:
 		rtm->rtm_addrs |= RTA_DST;
 	}
