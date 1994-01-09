c8bc955d2b2e971fd9554b54f96764e512a29a8a
Keith Bostic <bostic>
1994-01-09 14:37:03 -0800

    addbytes doesn't need termios.h any more.

--- lib/libcurses/addbytes.c
+++ lib/libcurses/addbytes.c
@@ -6,11 +6,10 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)addbytes.c	8.1 (Berkeley) 6/11/93";
+static char sccsid[] = "@(#)addbytes.c	8.2 (Berkeley) 1/9/94";
 #endif	/* not lint */
 
 #include <curses.h>
-#include <termios.h>
 
 #define	SYNCH_IN	{y = win->cury; x = win->curx;}
 #define	SYNCH_OUT	{win->cury = y; win->curx = x;}
