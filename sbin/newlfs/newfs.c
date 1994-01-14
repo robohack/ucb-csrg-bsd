01d2d105b1a53ba27bda5cdc9bba5a3ccd9a6d41
Keith Bostic <bostic>
1994-01-13 17:47:46 -0800

    usage should say newlfs, not newfs

--- sbin/newlfs/newfs.c
+++ sbin/newlfs/newfs.c
@@ -12,7 +12,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)newfs.c	8.1 (Berkeley) 6/5/93";
+static char sccsid[] = "@(#)newfs.c	8.2 (Berkeley) 1/13/94";
 #endif /* not lint */
 
 /*
@@ -402,7 +402,7 @@ usage()
 		    "usage: mfs [ -fsoptions ] special-device mount-point\n");
 	} else
 		fprintf(stderr,
-		    "usage: newfs [ -fsoptions ] special-device%s\n",
+		    "usage: newlfs [ -fsoptions ] special-device%s\n",
 #ifdef COMPAT
 		    " [device-type]");
 #else
