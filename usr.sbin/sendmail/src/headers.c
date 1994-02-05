f7532935809f93b46a7970049f18924ed368cacf
Eric Allman <eric>
1994-02-05 12:29:42 -0800

    allow $s to be set to NULL for locally generated mail

--- usr.sbin/sendmail/src/headers.c
+++ usr.sbin/sendmail/src/headers.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)headers.c	8.24 (Berkeley) 2/3/94";
+static char sccsid[] = "@(#)headers.c	8.25 (Berkeley) 2/5/94";
 #endif /* not lint */
 
 # include <errno.h>
@@ -504,6 +504,8 @@ logsender(e, msgid)
 		name = "[RESPONSE]";
 	else if ((name = macvalue('_', e)) != NULL)
 		;
+	else if (RealHostName == NULL)
+		name = "localhost";
 	else if (RealHostName[0] == '[')
 		name = RealHostName;
 	else
