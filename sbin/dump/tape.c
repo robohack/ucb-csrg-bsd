5176b12d5aed353eb426faa5019320eddb51468a
Keith Bostic <bostic>
1994-03-17 05:36:38 -0800

    When rdump aborts, it doesn't always properly close a remote tape. Consider an example: remote tape error.  tperror() gets called. If the user decides to punt the dump, it's aborted, and the remote is never properly closed. From: "Chris G. Demetriou" <cgd@sun-lamp.cs.berkeley.edu>

--- sbin/dump/tape.c
+++ sbin/dump/tape.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)tape.c	8.1 (Berkeley) 6/5/93";
+static char sccsid[] = "@(#)tape.c	8.2 (Berkeley) 3/17/94";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -616,6 +616,9 @@ dumpabort(signo)
 		killall();
 		msg("The ENTIRE dump is aborted.\n");
 	}
+#ifdef RDUMP
+	rmtclose();
+#endif
 	Exit(X_ABORT);
 }
 
