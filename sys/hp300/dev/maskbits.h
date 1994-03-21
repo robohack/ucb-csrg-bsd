2ec96502b5e1a60918c1b7b2993426b900ceaab0
Keith Bostic <bostic>
1994-03-21 13:50:27 -0800

    add add a copyright  noitce

--- sys/hp300/dev/maskbits.h
+++ sys/hp300/dev/maskbits.h
@@ -1,3 +1,12 @@
+/*-
+ * Copyright (c) 1994
+ *	The Regents of the University of California.  All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)maskbits.h	8.2 (Berkeley) 3/21/94
+ */
+
 /*
  * Derived from X11R4
  */
