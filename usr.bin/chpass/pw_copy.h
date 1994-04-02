914e79524a52ac0e7d3b98f3e63fc3efaa43c043
Jan-Simon Pendry <pendry>
1994-04-02 09:10:06 -0800

    date and time created 94/04/02 09:10:06 by pendry

---
+++ usr.bin/chpass/pw_copy.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 1994
+ *	The Regents of the University of California.  All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)pw_copy.h	8.1 (Berkeley) 4/2/94
+ */
+
+void	 pw_copy __P((int, int, struct passwd *));
