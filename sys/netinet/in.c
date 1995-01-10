c5a758281a302141cfbf0e006f72557fd113e937
Chris G. Demetriou <cgd>
1995-01-09 18:19:38 -0800

    ioctl cmd arg is u_long

--- sys/netinet/in.c
+++ sys/netinet/in.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)in.c	8.3 (Berkeley) 11/7/94
+ *	@(#)in.c	8.4 (Berkeley) 1/9/95
  */
 
 #include <sys/param.h>
@@ -132,7 +132,7 @@ extern	struct ifnet loif;
 /* ARGSUSED */
 in_control(so, cmd, data, ifp)
 	struct socket *so;
-	int cmd;
+	u_long cmd;
 	caddr_t data;
 	register struct ifnet *ifp;
 {
--- sys/netinet/tcp_usrreq.c
+++ sys/netinet/tcp_usrreq.c
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	@(#)tcp_usrreq.c	8.2 (Berkeley) 1/3/94
+ *	@(#)tcp_usrreq.c	8.3 (Berkeley) 1/9/95
  */
 
 #include <sys/param.h>
@@ -57,7 +57,7 @@ tcp_usrreq(so, req, m, nam, control)
 	int ostate;
 
 	if (req == PRU_CONTROL)
-		return (in_control(so, (int)m, (caddr_t)nam,
+		return (in_control(so, (u_long)m, (caddr_t)nam,
 			(struct ifnet *)control));
 	if (control && control->m_len) {
 		m_freem(control);
