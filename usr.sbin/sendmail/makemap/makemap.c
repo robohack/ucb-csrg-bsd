42d28c81fefecd5e1ea338200ab4af9e091d9f9d
Eric Allman <eric>
1995-02-03 08:10:01 -0800

    use size_t for xx.size for maximum compability (I hope)

--- usr.sbin/sendmail/makemap/makemap.c
+++ usr.sbin/sendmail/makemap/makemap.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)makemap.c	8.8 (Berkeley) 1/10/95";
+static char sccsid[] = "@(#)makemap.c	8.9 (Berkeley) 2/3/95";
 #endif /* not lint */
 
 #include <stdio.h>
@@ -41,7 +41,7 @@ union dbent
 	struct
 	{
 		char	*data;
-		int	size;
+		size_t	size;
 	} xx;
 };
 
