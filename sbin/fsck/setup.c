082ca81ac9872a72122d3fe9091d0bebc156b350
Kirk McKusick <mckusick>
1995-03-21 00:10:40 -0800

    daddr_t => ufs_daddr_t

--- sbin/fsck/setup.c
+++ sbin/fsck/setup.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)setup.c	8.6 (Berkeley) 1/6/95";
+static char sccsid[] = "@(#)setup.c	8.7 (Berkeley) 3/21/95";
 #endif /* not lint */
 
 #define DKTYPENAMES
@@ -272,7 +272,7 @@ badsb:
 readsb(listerr)
 	int listerr;
 {
-	daddr_t super = bflag ? bflag : SBOFF / dev_bsize;
+	ufs_daddr_t super = bflag ? bflag : SBOFF / dev_bsize;
 
 	if (bread(fsreadfd, (char *)&sblock, super, (long)SBSIZE) != 0)
 		return (0);
