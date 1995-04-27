26f93804eb154b21d827ca77db10cb93894f959f
Keith Bostic <bostic>
1995-04-27 13:20:47 -0800

    POSIX.2 says that a successful variable assignment should set the status variable ($?) to 0. From: "J.T. Conklin" <jtc@sun-lamp.cs.berkeley.edu>

--- bin/sh/eval.c
+++ bin/sh/eval.c
@@ -9,7 +9,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)eval.c	8.2 (Berkeley) 4/27/95";
+static char sccsid[] = "@(#)eval.c	8.3 (Berkeley) 4/27/95";
 #endif /* not lint */
 
 /*
@@ -841,7 +841,7 @@ prehash(n)
 
 bltincmd(argc, argv)  char **argv; {
 	listsetvar(cmdenviron);
-	return exitstatus;
+	return 0;
 }
 
 
