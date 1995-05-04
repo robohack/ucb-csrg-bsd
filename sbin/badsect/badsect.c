2daa1a14d92f54ed1a582a71fe6ec895f94d80b4
Kirk McKusick <mckusick>
1995-05-04 15:25:20 -0800

    include ordering

--- sbin/badsect/badsect.c
+++ sbin/badsect/badsect.c
@@ -12,7 +12,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)badsect.c	8.1 (Berkeley) 6/5/93";
+static char sccsid[] = "@(#)badsect.c	8.2 (Berkeley) 5/4/95";
 #endif /* not lint */
 
 /*
@@ -29,8 +29,8 @@ static char sccsid[] = "@(#)badsect.c	8.1 (Berkeley) 6/5/93";
 #include <sys/dir.h>
 #include <sys/stat.h>
 
-#include <ufs/ffs/fs.h>
 #include <ufs/ufs/dinode.h>
+#include <ufs/ffs/fs.h>
 
 #include <fcntl.h>
 #include <paths.h>
