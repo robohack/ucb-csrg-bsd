e95cbd021d3f6cfde9fd63488f6a06a2a0d0f3af
Keith Bostic <bostic>
1995-05-04 14:46:14 -0800

    optarg/optind moved to unistd.h

--- usr.bin/tsort/tsort.c
+++ usr.bin/tsort/tsort.c
@@ -15,7 +15,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)tsort.c	8.2 (Berkeley) 3/30/94";
+static char sccsid[] = "@(#)tsort.c	8.3 (Berkeley) 5/4/95";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -28,6 +28,7 @@ static char sccsid[] = "@(#)tsort.c	8.2 (Berkeley) 3/30/94";
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 /*
  *  Topological sort.  Input is a list of pairs of strings separated by
