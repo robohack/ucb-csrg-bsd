45f0d88d3103c8d9f9a7003a082e78f7b68aeded
Christos Zoulas <christos>
1995-05-04 16:59:12 -0800

    date and time created 95/05/04 17:59:12 by christos

---
+++ bin/sh/arith.h
@@ -0,0 +1,11 @@
+/*-
+ * Copyright (c) 1995
+ *      The Regents of the University of California.  All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	@(#)arith.h	1.1 (Berkeley) 5/4/95
+ */
+
+int arith __P((char *));
+int expcmd __P((int , char **));
