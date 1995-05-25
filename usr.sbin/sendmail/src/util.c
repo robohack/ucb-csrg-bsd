0cb7f6f77bb81da94a1ae0c0e92722277451a214
Eric Allman <eric>
1995-05-25 14:54:43 -0800

    don't trash lines without trailing newlines

--- usr.sbin/sendmail/src/util.c
+++ usr.sbin/sendmail/src/util.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)util.c	8.69 (Berkeley) 5/24/95";
+static char sccsid[] = "@(#)util.c	8.70 (Berkeley) 5/25/95";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -1170,7 +1170,8 @@ fgetfolded(buf, n, f)
 	}
 	if (p == bp)
 		return (NULL);
-	*--p = '\0';
+	if (*--p == '\n')
+		*p = '\0';
 	return (bp);
 }
 /*
