89698d676213da0322adbc82b8f7d5a2c0510b9e
Eric Allman <eric>
1995-05-26 07:09:39 -0800

    beta.1

--- usr.sbin/sendmail/src/version.c
+++ usr.sbin/sendmail/src/version.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	8.7.Beta (Berkeley) 5/23/95";
+static char sccsid[] = "@(#)version.c	8.7.Beta (Berkeley) 5/26/95";
 #endif /* not lint */
 
-char	Version[] = "8.7.Beta.0";
+char	Version[] = "8.7.Beta.1";
