06ddf5d0881f0c17311dc31a1d7b7f9d78b7aa4c
Eric Allman <eric>
1995-05-29 17:13:42 -0800

    fix a comment

--- usr.sbin/sendmail/src/readcf.c
+++ usr.sbin/sendmail/src/readcf.c
@@ -7,7 +7,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)readcf.c	8.95 (Berkeley) 5/28/95";
+static char sccsid[] = "@(#)readcf.c	8.96 (Berkeley) 5/29/95";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -2073,8 +2073,7 @@ setclass(class, str)
 **		line -- the config file line
 **
 **	Returns:
-**		TRUE if it successfully entered the map entry.
-**		FALSE otherwise (usually syntax error).
+**		none.
 **
 **	Side Effects:
 **		Enters the map into the dictionary.
