57c5f46047d0e34337b8127c2dbf45fc7aec59d0
Eric Allman <eric>
1995-06-10 10:46:38 -0800

    ignore -t in -bd mode

--- usr.sbin/sendmail/src/main.c
+++ usr.sbin/sendmail/src/main.c
@@ -13,7 +13,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	8.122 (Berkeley) 6/10/95";
+static char sccsid[] = "@(#)main.c	8.123 (Berkeley) 6/10/95";
 #endif /* not lint */
 
 #define	_DEFINE
@@ -762,6 +762,7 @@ main(argc, argv, envp)
 	  case MD_DAEMON:
 		/* remove things that don't make sense in daemon mode */
 		FullName = NULL;
+		GrabTo = FALSE;
 
 		/* arrange to restart on hangup signal */
 		setsignal(SIGHUP, sighup);
