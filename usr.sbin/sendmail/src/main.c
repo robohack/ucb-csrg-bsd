9bd28695efb66e8de17d4284af18de768e6c0f53
Eric Allman <eric>
1995-06-18 10:53:16 -0800

    add class `m' to include message subtypes that can be treated as though rfc822

--- usr.sbin/sendmail/src/main.c
+++ usr.sbin/sendmail/src/main.c
@@ -13,7 +13,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	8.131 (Berkeley) 6/17/95";
+static char sccsid[] = "@(#)main.c	8.132 (Berkeley) 6/18/95";
 #endif /* not lint */
 
 #define	_DEFINE
@@ -865,6 +865,9 @@ main(argc, argv, envp)
 
 	/* MIME Content-Types that cannot be transfer encoded */
 	setclass('n', "multipart/signed");
+
+	/* MIME message/* subtypes that can be treated as messages */
+	setclass('m', "rfc822");
 
 	/* MIME Content-Transfer-Encodings that can be encoded */
 	setclass('e', "7bit");
