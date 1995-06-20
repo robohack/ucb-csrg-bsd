4c616276216b7e461b8e4ae873d4ccdc4ef628be
Eric Allman <eric>
1995-06-20 08:37:19 -0800

    avoid infinite loop on /tryflags

--- usr.sbin/sendmail/src/main.c
+++ usr.sbin/sendmail/src/main.c
@@ -13,7 +13,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	8.134 (Berkeley) 6/20/95";
+static char sccsid[] = "@(#)main.c	8.135 (Berkeley) 6/20/95";
 #endif /* not lint */
 
 #define	_DEFINE
@@ -1797,7 +1797,7 @@ testmodeline(line, e)
 		}
 		else if (strcasecmp(&line[1], "tryflags") == 0)
 		{
-			while (*p != '\n')
+			for (; *p != '\0'; p++)
 			{
 				switch (*p)
 				{
